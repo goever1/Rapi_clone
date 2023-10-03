657b9045b7db415cfff2c316e232cfd02be9274b
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-10-03 14:06:12 -0500

    Createtables.sql

---
+++ Createtables.sql
@@ -0,0 +1,21 @@
+USE rappidb;
+CREATE TABLE TipoComida (
+    TipoComidaID INT AUTO_INCREMENT PRIMARY KEY,
+    NombreTipoComida VARCHAR(255) NOT NULL
+);
+CREATE TABLE Comida (
+    ComidaID INT AUTO_INCREMENT PRIMARY KEY,
+    NombreComida VARCHAR(255) NOT NULL,
+    Descripcion TEXT,
+    Precio DECIMAL(10, 2) NOT NULL,
+    TipoComidaID INT,
+    RestauranteID INT,
+    FOREIGN KEY (TipoComidaID) REFERENCES TipoComida(TipoComidaID),
+    FOREIGN KEY (RestauranteID) REFERENCES Restaurante(RestauranteID)
+);
+CREATE TABLE Restaurante (
+    RestauranteID INT AUTO_INCREMENT PRIMARY KEY,
+    NombreRestaurante VARCHAR(255) NOT NULL,
+    Direccion VARCHAR(255) NOT NULL,
+    Telefono VARCHAR(20),
+);
